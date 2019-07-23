870114ba7452383462be5062b3fcbf78c6c3be47
maltimore <malte.esders@gmail.com>
2019-07-23 17:36:35 +0200

    made it possible to choose which main.py file should be run

--- SGE/SGE_entry.sh
+++ SGE/SGE_entry.sh
@@ -19,4 +19,5 @@
 
 # $1 is the directory that this script is in. We get it passed as a cli argument by the calling script,
 # because the SGE_entry script is not *actually* in this directory. Qsub puts this file into another directory.
-python "$1"/python_entry.py
+# $2 is the directory that the main.py file of the program we're supposed to run is in
+python "$1"/python_entry.py "$2"
