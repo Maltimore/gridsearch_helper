67054d12a15ae5f79872958cf1bf01d4c7cbe795
maltimore <malte.esders@gmail.com>
2019-07-24 11:10:18 +0200

    added __init__.py and setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gridsearch_analysis",
+    version="0.0.0",
+    author="Malte Esders",
+    description="Analysis of gridsearch performed with gridsearch_helper",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/maltimore/gridsearch_helper",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: custom non-military",
+        "Operating System :: OS Independent",
+    ],
+)
