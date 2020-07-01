6edc98f5761298470bf9cd2f392ce6e065b8cce7
Maltimore <git@maltimore.info>
2020-07-01 12:27:35 +0200

    gridsearch_entry.sh: check whether output directory already exists and give the option to abort

--- SGE/gridsearch_entry.sh
+++ SGE/gridsearch_entry.sh
@@ -24,12 +24,25 @@ taskrange_begin="$2"
 taskrange_end="$3"
 job_name="$4"
 
-# get the full path to directory of the python main file
+# get the full path to the python main file with readlink
 python_main_file=$(readlink -f $python_main_file)
 python_main_file_dir=$(dirname "$python_main_file")
+output_dir=./outfiles/"$job_name"
+
+# If the output directory already exists, warn and ask whether we should exit
+if [ -d "$output_dir" ]; then
+    echo ""
+    echo "Output directory already exists. You likely started a gridsearch with the same name as a previous one."
+    echo "If you continue with this name for the gridsearch, the output files are going to be written into the existing directory."
+    echo "Continue with this name for the gridsearch? [y/N]"
+    read answer
+    if [ "$answer" != "y" ]; then
+        echo "Exiting, no gridsearch started"
+        exit 1
+    fi
+fi
 
 # make output directory
-output_dir=./outfiles/"$job_name"
 mkdir --parents "$output_dir"
 
 # copy parameters file into output directory
@@ -37,9 +50,12 @@ cp "$python_main_file_dir"/parameters.yaml "$output_dir"/parameters.yaml
 
 # Clear text output directory? (Ask only if it exists)
 if [ -d "./textoutput/$job_name" ]; then
-	echo "Shall I clear the folders with output and error text? [y/n] (default: y)"
+    echo ""
+    echo "There is already a stdin/stderr directory at the location where we would put the stdin/stdout files for this new grisearch."
+    echo "If the stdin/stderr files from the previous gridsearch are not removed, the ones from this one will be appended to the old ones."
+    echo "If the previous gridsearch is still running, there is a chance that some of its jobs will crash if you temporarily remove the directory."
+	echo "Shall I remove and re-create the textoutput folder? [y/n] (default: y)"
     read answer
-
     if [ "$answer" == "y" ] || [ "$answer" == "" ]; then
         echo "Clearing.."
         rm -rf ./textoutput/"$job_name"
@@ -48,6 +64,7 @@ fi
 
 # if textoutput directory doesn't exist, create it
 if [ ! -d "./textoutput/$job_name" ]; then
+    echo ""
 	echo "Creating textoutput directory textoutput"
 	mkdir --parents ./textoutput/"$job_name"
 fi
