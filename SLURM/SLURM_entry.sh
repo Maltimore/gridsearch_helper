1770849960cf2f4d6f903687fbe124bdeec63b63
Maltimore <git@maltimore.info>
2025-07-24 14:05:01 +0200

    minor code cleanup

--- SLURM/SLURM_entry.sh
+++ SLURM/SLURM_entry.sh
@@ -1,14 +1,13 @@
 #! /bin/bash
 
 echo $(date) "Now in SLURM_entry.sh"
-
 echo $(date) "Current working directory: $(pwd)"
 echo $(date) "Hostname: $(hostname)"
 
 # DO NOT MESS WITH THE VARIABLES RANDOM_SEED AND OUTPUT_PATH
 export RANDOM_SEED="$RANDOM"
 # we are accessing here the OUTPUT_PATH variable that is set in slurmlaunch
-# and modify if with the info of the current job
+# and modify it with the info of the current job
 OUTPUT_PATH+="/job_outputs/$(printf %05d "$SLURM_ARRAY_TASK_ID")"
 echo $(date) "The output path is ${OUTPUT_PATH}"
 
@@ -17,11 +16,11 @@ export RANDOM_SEED="$RANDOM"
 
 # gather information about the current environment and save it to run_info.yaml
 echo $(date) running launch script
-"$LAUNCH_SCRIPT" --action setup  --output-path="${OUTPUT_PATH}"
+"${LAUNCH_SCRIPT}" --action setup  --output-path="${OUTPUT_PATH}"
 echo $(date) Starting main program
 
 ########################################################################################################################
-# here you can put the commands you would like to run
+# only here you should put the commands you would like to run
 SCRIPT='scripts/my_example_script.py'
 echo $(date) Running ${SCRIPT}
 python ${SCRIPT} --output_path="${OUTPUT_PATH}" --params_path="${PARAMS_PATH}" --random_seed="${RANDOM_SEED}"
@@ -29,4 +28,4 @@ python ${SCRIPT} --output_path="${OUTPUT_PATH}" --params_path="${PARAMS_PATH}" -
 
 # finish the job by putting the end time into the run_info.yaml
 echo $(date) running cleanup script
-"$LAUNCH_SCRIPT" --action cleanup  --output-path="${OUTPUT_PATH}"
+"${LAUNCH_SCRIPT}" --action cleanup  --output-path="${OUTPUT_PATH}"
